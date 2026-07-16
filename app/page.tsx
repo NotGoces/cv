b93b5c4e2dd20376260c55eac96b52119d763a4d
NotGoces <gocesmusic@gmail.com>
2026-07-16 13:42:26 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Skills from "./components/Skills";
-import Projects from "./components/projects";
+import Projects from "./components/Projects";
 import Education from "./components/Education";
 import Contact from "./components/Contact";
-import Footer from "./components/footer";
+import Footer from "./components/Footer";
 import { Lang, createT } from "./constants/translations";
 
 export default function Home() {
